cd653a74a356e4b4796855ac69da3363877b5603
Dragos Surugiu <dragoss@dragoss>
2023-01-11 12:39:33 +0200

    Added udm dir and cmake, updated scripts to work everywhere

--- diff.sh
+++ diff.sh
@@ -1,14 +1,20 @@
+#!/bin/sh
+
 current_dir=$PWD;
 
 cd build/;
 
 
-MACHINE_TYPE=`uname -m`
-if [ "$MACHINE_TYPE" -eq "arm64" ]
+MACHINE_TYPE=$(uname -m)
+echo "Found the folowing arch: <$MACHINE_TYPE>"
+
+if [ "${MACHINE_TYPE}" == "arm64" ]
 then
     cmake ../ -Darch:String="arm64";
-else
-    cmake ../ -Darch:String="x86";
+elif [ "${MACHINE_TYPE}" == "x86_64" ]
+then
+    #cmake ../ -Darch:String="x86" -Dlinuxvm:BOOL=OFF;
+    cmake ../ -Darch:String="x86" -Dwork:STRING="ON";
 fi
 
 if cmake --build .; then 
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 current_dir=$PWD;
 
 rm -rf build;
@@ -5,12 +7,16 @@ mkdir build;
 cd build/;
 
 
-MACHINE_TYPE=`uname -m`
-if [ "$MACHINE_TYPE" eq "arm64" ]
+MACHINE_TYPE=$(uname -m)
+echo "Found the folowing arch: <$MACHINE_TYPE>"
+
+if [ "${MACHINE_TYPE}" == "arm64" ]
 then
     cmake ../ -Darch:String="arm64";
-else
-    cmake ../ -Darch:String="x86";
+elif [ "${MACHINE_TYPE}" == "x86_64" ]
+then
+    #cmake ../ -Darch:String="x86" -Dwork:STRING="OFF";
+    cmake ../ -Darch:String="x86" -Dwork:STRING="ON";
 fi
 
 if cmake --build .; then 
